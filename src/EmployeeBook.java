a1a46a24f9809850e17c63dbf318f1aec8f0cc91
Василий <remindscope@gmail.com>
2022-12-14 13:41:01 +0300

    Added new exception for failed search; fixed a bug which allowed employee being moved to 0 department; output design improvement; added examples in Main class

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,19 +1,21 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 class EmployeeBook {
-    private ArrayList<Employee> theBook = new ArrayList<>();
+    private ArrayList<Employee> theBook = new ArrayList<>(); // Массив сотрудников
     private int id = 0; // Счетчик id
+    private DecimalFormat moneyFormat = new DecimalFormat("###,###.##"); // Определение формата для денег
 
     public EmployeeBook() {
     }
 
-    public void sumSalary(int department) { // Сумма ЗП: выводит результат работы метода salarySumming //+
+    public void sumSalary(int department) { // Сумма ЗП: выводит результат работы метода summingSalary (среди всех или отдела)
         checkDepartment(department);
-        String lineFiler = department == 0 ? "all employees" : department + " department";
-        System.out.printf("Salary sum of %s is: %s\n", lineFiler, summingSalary(department));
+        String filler = department == 0 ? "all employees" : department + " department";
+        System.out.printf("Salary sum of %s is: %s\n", filler, moneyFormat.format(summingSalary(department)));
     }
 
-    public void findAverageSalary(int department) { //Средняя ЗП //+
+    public void findAverageSalary(int department) { // Средняя ЗП (среди всех или отдела)
         checkDepartment(department);
         int peoplesAmount = 0;
         for (int i = 0; i < theBook.size(); i++) {
@@ -21,11 +23,11 @@ class EmployeeBook {
                 peoplesAmount++;
             }
         }
-        String lineFiller = department == 0 ? "all employees" : department + " department";
-        System.out.printf("Average salary of %s is %.2f\n", lineFiller, summingSalary(department) / peoplesAmount);
+        String filler = department == 0 ? "all employees" : department + " department";
+        System.out.printf("Average salary of %s is: %s\n", filler, moneyFormat.format(summingSalary(department) / peoplesAmount));
     }
 
-    public void findSmallestSalary(int department) { // Min ЗП 1-5 конкретный департамент, 0 - всe // +
+    public void findSmallestSalary(int department) { // Ищет самую низкую ЗП (среди всех или отдела)
         checkDepartment(department);
         double minimal = theBook.get(0).getSalary();
         for (int i = 0; i < theBook.size(); i++) {
@@ -35,10 +37,11 @@ class EmployeeBook {
                 }
             }
         }
-        System.out.println(minimal);
+        String filler = department == 0 ? "overall" : "in department " + department;
+        System.out.printf("The smallest salary %s is: %s\n", filler, moneyFormat.format(minimal));
     }
 
-    public void findBiggestSalary(int department) { // Max ЗП 1-5 конкретный департамент, 0 - всe // +
+    public void findBiggestSalary(int department) { // Ищет самую большую ЗП (среди всех или отдела)
         checkDepartment(department);
         double max = theBook.get(0).getSalary();
         for (int i = 0; i < theBook.size(); i++) {
@@ -48,63 +51,68 @@ class EmployeeBook {
                 }
             }
         }
-        System.out.println(max);
+        String filler = department == 0 ? "overall" : "in department " + department;
+        System.out.printf("The biggest salary %s is: %s\n", filler, moneyFormat.format(max));
     }
 
-    public void printmployeesDataList(int department) { // Список данных сотрудников (всех или отдела) // +
+    public void printEmployeesDataList(int department) { // Список данных сотрудников (всех или отдела)
         checkDepartment(department);
+        System.out.println(department == 0 ? "All employees data:" : department + " department employees data:");
         for (int i = 0; i < theBook.size(); i++) {
             if (theBook.get(i).getDepartment() == department || department == 0) {
-                String lineFiller = department == 0 ? "Department: " + department + "," : "";
-                System.out.printf("%s id: %s, Name: %s, Salary: %s\n", lineFiller, theBook.get(i).getId(), theBook.get(i).getName(), theBook.get(i).getSalary());
+                System.out.printf("  id: %s,  %s, salary: %s\n", theBook.get(i).getId(), theBook.get(i).getName(), moneyFormat.format(theBook.get(i).getSalary()));
             }
         }
     }
 
-    public void printEmployeesNamesList(int department) { // Вывод имен сотрудников (всех или отдела) // +
+    public void printEmployeesNamesList(int department) { // Вывод имен сотрудников (всех или отдела)
         checkDepartment(department);
+        System.out.println(department == 0 ? "All employees names list:" : department + " department employees names list:");
         for (int i = 0; i < theBook.size(); i++) {
             if (theBook.get(i).getDepartment() == department || department == 0) {
-                System.out.printf("Name: %s\n", theBook.get(i).getName());
+                System.out.println("  " + theBook.get(i).getName());
             }
         }
     }
 
-    public void indexingSalary(int department, double percent) { // Индексация ЗП (всех или отдела) // +
+    public void indexingSalary(int department, double percent) { // Индексация ЗП (всех или отдела)
         checkDepartment(department);
         for (int i = 0; i < theBook.size(); i++) {
             if (theBook.get(i).getDepartment() == department || department == 0) {
                 theBook.get(i).setSalary(theBook.get(i).getSalary() + theBook.get(i).getSalary() * percent / 100);
             }
         }
-        System.out.println("Indexation completed");
+        String filler = department == 0 ? "to all employees" : "to department " + department;
+        System.out.println("Indexation applied " + filler);
     }
 
-    public void printListWithSalaryBelow(double salaryBorder) { // Выводит всех, у кого ЗП меньше границы // +
+    public void printListWithSalaryBelow(double salaryBorder) { // Выводит всех, у кого ЗП меньше границы
         checkNegative(salaryBorder);
+        System.out.printf("These employees have salary below %s:\n", moneyFormat.format(salaryBorder));
         for (int i = 0, switcher = 1; i < theBook.size(); i++) {
             if (theBook.get(i).getSalary() < salaryBorder) {
-                System.out.printf("This employee has salary below %s: %s, %s, %s\n", salaryBorder, theBook.get(i).getId(), theBook.get(i).getName(), theBook.get(i).getSalary());
+                System.out.printf("  id: %s, %s, salary: %s\n", theBook.get(i).getId(), theBook.get(i).getName(), moneyFormat.format(theBook.get(i).getSalary()));
                 switcher = 0; // отменяет выполнение else на последнем элементе массива, если if выполнилось хотя бы раз
             } else if (i == (theBook.size() - 1) * switcher) {
-                System.out.printf("No one has salary smaller than %s\n", salaryBorder);
+                System.out.printf("No one has salary smaller than %s\n", moneyFormat.format(salaryBorder));
             }
         }
     }
 
-    public void getListWithSalaryAbove(double salaryBorder) { // Выводит всех, у кого ЗП выше границы // +
+    public void printListWithSalaryAbove(double salaryBorder) { // Выводит всех, у кого ЗП выше границы
         checkNegative(salaryBorder);
+        System.out.printf("These employees have salary above %s:\n", moneyFormat.format(salaryBorder));
         for (int i = 0, switcher = 1; i < theBook.size(); i++) {
             if (theBook.get(i).getSalary() >= salaryBorder) {
-                System.out.printf("This employee has salary above %s: %s, %s, %s\n", salaryBorder, theBook.get(i).getId(), theBook.get(i).getName(), theBook.get(i).getSalary());
+                System.out.printf("  id: %s, %s, salary: %s\n", theBook.get(i).getId(), theBook.get(i).getName(), moneyFormat.format(theBook.get(i).getSalary()));
                 switcher = 0; // отменяет выполнение else на последнем элементе массива, если if выполнилось хотя бы раз
             } else if (i == (theBook.size() - 1) * switcher) {
-                System.out.printf("No one has salary bigger than %s\n", salaryBorder);
+                System.out.printf("No one has salary bigger than %s\n", moneyFormat.format(salaryBorder));
             }
         }
     }
 
-    public void addNewEmployee(String name, double salary, int department) { // Добавляет сотрудника // +
+    public void addNewEmployee(String name, double salary, int department) { // Добавляет сотрудника
         checkName(name);
         checkNegative(salary);
         checkDepartment(department);
@@ -114,48 +122,55 @@ class EmployeeBook {
         id++;
     }
 
-    public void removeEmployee(String name) { // Убирает сотрудника через имя // +
+    public void removeEmployee(String name) { // Убирает сотрудника через имя
         for (int i = 0; i < theBook.size(); i++) {
             if (name.equals(theBook.get(i).getName())) {
                 theBook.remove(i);
+                System.out.println("Employee " + name + " has been deleted");
+                return;
             }
         }
-        System.out.println("Employee has been deleted");
+        searchFail("name");
     }
 
-    public void removeEmployee(int id) { // Убирает сотрудника через id // +
+    public void removeEmployee(int id) { // Убирает сотрудника через id
         checkNegative(id);
         for (int i = 0; i < theBook.size(); i++) {
             if (id == theBook.get(i).getId()) {
+                System.out.printf("Employee %s has been deleted\n", theBook.get(i).getName());
                 theBook.remove(i);
+                return;
             }
         }
-        System.out.println("Employee has been deleted");
+        searchFail("id");
     }
 
-    public void changeSalary(String name, double newSalary) { // Меняет ЗП по имени // +
+    public void changeSalary(String name, double newSalary) { // Меняет ЗП по имени
         checkName(name);
         checkNegative(newSalary);
         for (int i = 0; i < theBook.size(); i++) {
             if (name.equals(theBook.get(i).getName())) {
                 theBook.get(i).setSalary(newSalary);
+                System.out.printf("New salary for %s has been set: %s\n", name, moneyFormat.format(newSalary));
+                return;
             }
         }
-        System.out.printf("New salary for %s has been set: %s\n", name, newSalary);
+        searchFail("name");
     }
 
-    public void changeDepartment(String name, int newDepartment) { // Меняет отдел по имени // +
+    public void changeDepartment(String name, int newDepartment) { // Меняет отдел по имени
         checkName(name);
         checkDepartment(newDepartment);
         for (int i = 0; i < theBook.size(); i++) {
             if (name.equals(theBook.get(i).getName())) {
                 theBook.get(i).setDepartment(newDepartment);
+                System.out.printf("New department for %s is now %s\n", name, newDepartment);
             }
         }
-        System.out.printf("New department for %s is now %s\n", name, newDepartment);
+        searchFail("name");
     }
 
-    private double summingSalary(int department) { // считает сумму ЗП (всех или отдела) // +
+    private double summingSalary(int department) { // считает сумму ЗП (всех или отдела)
         checkDepartment(department);
         double salarySumCurrent = 0;
         for (int i = 0; i < theBook.size(); i++) {
@@ -165,11 +180,12 @@ class EmployeeBook {
         }
         return salarySumCurrent;
     }
-/////// Блок проверки данных
-    private void checkDepartment(int department) { // Проверяет значение департамента. Не дает создать сотрудника с департаментом 0
+
+    /////// Блок проверки данных
+    private void checkDepartment(int department) { // Проверяет значение департамента и не позволяет иметь сотруднику департамент 0
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        if ((stackTrace[2].getMethodName() == "addNewEmployee") && department > 5 || department <= 0) {
-            throw new IllegalArgumentException("Department must be from 1 to 5! can't add a new employee with department value " + department);
+        if ((stackTrace[2].getMethodName().equals("addNewEmployee") || stackTrace[2].getMethodName().equals("changeDepartment")) && (department > 5 || department <= 0)) {
+            throw new IllegalArgumentException("Department must be from 1 to 5! Employee can't get department " + department);
         }
         if (department > 5 || department < 0) {
             throw new IllegalArgumentException("Department must be from 1 to 5! can't operate with department value " + department + ". Use 1-5 for employee operations or 0 for general operations");
@@ -181,11 +197,17 @@ class EmployeeBook {
             throw new IllegalArgumentException("Negative value! salary and id must always be positive");
         }
     }
-    private void checkName(String name) { // Проверяет ФИО на соответствие формату
+
+    private void checkName(String name) { // Проверяет ФИО на соответствие формату Фамилиия Имя Отчество
         if (!name.matches("[А-Я][а-я]+(-?[А-Я][а-я]+)*\\s[А-Я][а-я]+\\s[А-Я][а-я]+")) {
             throw new IllegalArgumentException("Wrong name! Use russian keys with format: Фамилия Имя Отчество");
         }
     }
+
+    private void searchFail(String filler) { // Сообщение об ошибке при остутствии записи в БД
+        throw new IllegalArgumentException("This employee does not exist! Make sure that " + filler + " is correct!");
+    }
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,36 @@ public class Main {
             printEmployeesNamesList - Gives all company's or single department names
             indexingSalary          - Set new salaries for everyone or single department via index
             printListWithSalaryBelow- Shows list of employees of company or single department with salary below value
-            getListWithSalaryAbove  - Shows list of employees of company or single department with salary above value
+            printListWithSalaryAbove- Shows list of employees of company or single department with salary above value
             removeEmployee          - Removes employee from database via name or id
             changeSalary            - Changes the salary via name
             changeDepartment        - Changes department via name
             ------------------------------------------------------------------------------------------------------
          */
-        EmployeeBook aBook = new EmployeeBook();
+        EmployeeBook eBook = new EmployeeBook();
+        eBook.addNewEmployee("Пирожков Артур Максимович", 45000, 4);
+        eBook.addNewEmployee("Куклачев Юрий Дмитриевич", 90000, 1);
+        eBook.addNewEmployee("Бэкхем Дэвид Эдвардович", 10000000, 3);
+        eBook.addNewEmployee("Салтыков-Щедрин Михаил Еврграфович", 2000, 4);
+        eBook.addNewEmployee("Киркоров Филипп Бедросович", 300000, 2);
+        eBook.addNewEmployee("Галифианакис Зак Гарриевич", 200000, 2);
+        eBook.addNewEmployee("Сагиев Борат Александрович", 15, 2);
+        eBook.sumSalary(0);
+        eBook.sumSalary(4);
+        eBook.findAverageSalary(0);
+        eBook.findAverageSalary(4);
+        eBook.findSmallestSalary(4);
+        eBook.findBiggestSalary(0);
+        eBook.printEmployeesDataList(2);
+        eBook.printEmployeesNamesList(3);
+        eBook.printEmployeesNamesList(0);
+        eBook.indexingSalary(1, 10);
+        eBook.printListWithSalaryBelow(20);
+        eBook.printListWithSalaryAbove(150000);
+        eBook.removeEmployee("Киркоров Филипп Бедросович");
+        eBook.printEmployeesDataList(1);
+        eBook.removeEmployee(1);
+        eBook.sumSalary(1);
+        eBook.changeSalary("Галифианакис Зак Гарриевич", 250000);
     }
 }
