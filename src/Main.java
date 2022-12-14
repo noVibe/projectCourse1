d7d2bbbfb570bf7f4a40a815adc3e558e40ebd0c
Василий <remindscope@gmail.com>
2022-12-15 01:29:19 +0300

    Refactoring of department logic and checking valid ones     Now department amount can be set for every array. Department checking can be tweaked in every case

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 // Use department 0 for all database operations. Use 1-5 for single department operations
 public class Main {
     public static void main(String[] args) {
-        EmployeeBook eBook = new EmployeeBook();
-        eBook.addNewEmployee("Пирожков Артур Максимович", 45000, 4);
+        EmployeeBook eBook = new EmployeeBook(5);
+        eBook.addNewEmployee("Пирожков Артур Максимович", 45000, 1);
         eBook.addNewEmployee("Куклачев Юрий Дмитриевич", 90000, 1);
         eBook.addNewEmployee("Бэкхем Дэвид Эдвардович", 10000000, 3);
         eBook.addNewEmployee("Салтыков-Щедрин Михаил Еврграфович", 2000, 4);
@@ -11,7 +11,7 @@ public class Main {
         eBook.addNewEmployee("Сагиев Борат Александрович", 15, 2);
         eBook.sumSalary(0);
         eBook.sumSalary(4);
-        eBook.findAverageSalary(0);
+        eBook.findAverageSalary(5);
         eBook.findAverageSalary(4);
         eBook.findSmallestSalary(4);
         eBook.findBiggestSalary(0);
@@ -23,7 +23,7 @@ public class Main {
         eBook.printListWithSalaryAbove(150000);
         eBook.removeEmployee("Киркоров Филипп Бедросович");
         eBook.printEmployeesDataList(1);
-        eBook.removeEmployee(1);
+        eBook.removeEmployee(0);
         eBook.sumSalary(1);
         eBook.changeSalary("Галифианакис Зак Гарриевич", 250000);
         eBook.printEmployeesDataList(0);
