40ca537a24585cbbb906de9a8d89762ab82d369e
Василий <remindscope@gmail.com>
2022-12-22 17:12:11 +0300

    Added method to control departments amount

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,13 @@ public class Main {
         eBook.addNewEmployee("Пирожков Артур Максимович", 45000, 1);
         eBook.addNewEmployee("Куклачев Юрий Дмитриевич", 90000, 1);
         eBook.addNewEmployee("Бэкхем Дэвид Эдвардович", 10000000, 3);
-        eBook.addNewEmployee("Салтыков-Щедрин Михаил Еврграфович", 2000, 4);
+        eBook.addNewEmployee("Салтыков-Щедрин Михаил Еврграфович", 200200, 4);
         eBook.addNewEmployee("Киркоров Филипп Бедросович", 300000, 2);
         eBook.addNewEmployee("Галифианакис Зак Гарриевич", 200000, 2);
         eBook.addNewEmployee("Сагиев Борат Александрович", 15, 2);
-        eBook.sumSalary(0);
-        eBook.sumSalary(4);
-        eBook.findAverageSalary(5);
-        eBook.findAverageSalary(4);
+
         eBook.findSmallestSalary(4);
-        eBook.findBiggestSalary(0);
-        eBook.printEmployeesDataList(2);
-        eBook.printEmployeesNamesList(3);
-        eBook.printEmployeesNamesList(0);
-        eBook.indexingSalary(1, 10);
-        eBook.printListWithSalaryBelow(20);
-        eBook.printListWithSalaryAbove(150000);
-        eBook.removeEmployee("Киркоров Филипп Бедросович");
-        eBook.printEmployeesDataList(1);
-        eBook.removeEmployee(0);
-        eBook.sumSalary(1);
-        eBook.changeSalary("Галифианакис Зак Гарриевич", 250000);
-        eBook.printEmployeesDataList(0);
-        eBook.modifyDepartments(3);
+
+
     }
 }
